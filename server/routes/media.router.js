d5994fea4d67b7755f4da35a0fd7f06ddb6a058f
Alex Gjorvad <alexgjorvad@gmail.com>
2018-08-03 19:20:08 -0500

    code edit

--- server/routes/media.router.js
+++ server/routes/media.router.js
@@ -12,13 +12,13 @@ router.get('/', (req, res) => {
         FROM "streets"
         ORDER BY "street_name" ASC;`
         pool.query(queryText)
-        .then((result) => {
-            console.log(result.rows)
-            res.send(result.rows);
-        }).catch((error) => {
-            console.log('error on GET: ', error);
-            res.sendStatus(500);
-        })
+            .then((result) => {
+                console.log(result.rows)
+                res.send(result.rows);
+            }).catch((error) => {
+                console.log('error on GET: ', error);
+                res.sendStatus(500);
+            })
     } else {
         res.sendStatus(403);
     }
@@ -26,16 +26,16 @@ router.get('/', (req, res) => {
 
 router.put('/', (req, res) => {
     console.log('PUT route', req.body);
-    const street=req.body;
-    if(req.isAuthenticated()) {
+    const street = req.body;
+    if (req.isAuthenticated()) {
         let queryText = `UPDATE "streets"
         SET "street_name" = $1, "street_history" = $2, "image_url" = $3, "link_url" = $4, "person_id" = $5
         WHERE "street_id" = $6;`;
         pool.query(queryText, [street.street_name, street.street_history, street.image_url, street.link_url, req.user.id, street.street_id])
-        .then((results) => {
-            res.sendStatus(200)
-            console.log('These are the results of the PUT', results);
-        })
+            .then((results) => {
+                res.sendStatus(200)
+                console.log('These are the results of the PUT', results);
+            })
     } else {
         res.sendStatus(403);
     }
@@ -46,19 +46,19 @@ router.delete('/', (req, res) => {
     console.log(req.query.street_id)
     // const street_id = req.params.street_id;
     if (req.isAuthenticated()) {
-    pool.query(`DELETE FROM "streets"
+        pool.query(`DELETE FROM "streets"
                         WHERE "street_id" = ($1);`, [req.query.street_id])
-        .then((results) => {
-            res.sendStatus(200);
-            console.log('These are the results of the DELETE', results)
-        })
-        .catch((error) => {
-            console.log('problem with DELETE from database', error);
-            res.sendStatus(500);
-        });
-    }else {
-            res.sendStatus(403);
-        }
+            .then((results) => {
+                res.sendStatus(200);
+                console.log('These are the results of the DELETE', results)
+            })
+            .catch((error) => {
+                console.log('problem with DELETE from database', error);
+                res.sendStatus(500);
+            });
+    } else {
+        res.sendStatus(403);
+    }
 });
 
 
